df75bcb461f2908dba7e325f841a75d9d4bbcb73
Taslim <tasleemoseni@gmail.com>
2017-09-29 10:09:35 +0100

    New layout changes

--- app/src/main/java/com/dabinu/apps/chesstimer/StartActivity.java
+++ app/src/main/java/com/dabinu/apps/chesstimer/StartActivity.java
@@ -7,29 +7,39 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
+import android.widget.Spinner;
 
 
 public class StartActivity extends AppCompatActivity {
 
     ListView list;
     ArrayAdapter<CharSequence> adapter;
+    Spinner hr, min;
+    ArrayAdapter<CharSequence> hrAdapter, minAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
+        hr = (Spinner) findViewById(R.id.hr);
+        min = (Spinner) findViewById(R.id.min);
+        hrAdapter = ArrayAdapter.createFromResource(this, R.array.hrs, R.layout.timespinner);
+        minAdapter = ArrayAdapter.createFromResource(this, R.array.mn, R.layout.timespinner);
+        hr.setAdapter(hrAdapter);
+        min.setAdapter(minAdapter);
+
+
         final Intent toTheSurgicalrRoom = new Intent(this, SurgicalRoom.class);
 
         list = (ListView) findViewById(R.id.listOfAll);
-        adapter = ArrayAdapter.createFromResource(this, R.array.time, android.R.layout.simple_spinner_item);
+        adapter = ArrayAdapter.createFromResource(this, R.array.time, R.layout.listitemmock);
         list.setAdapter(adapter);
 
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l){
-                String passed = null;
+                String passed;
                 switch(position){
                     case 0:
                         passed = Long.toString(1000 * 60 * 5);
